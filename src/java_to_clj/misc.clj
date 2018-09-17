98782d2ca9fafe3daa3e2dfa885057f8e7a264a8
David Young-chan Kay <davidykay@gmail.com>
2018-09-17 06:08:18 -0700

    Implemented basic comment support

--- src/java_to_clj/misc.clj
+++ src/java_to_clj/misc.clj
@@ -5,12 +5,24 @@
    [com.github.javaparser.ast
     body.VariableDeclarator
     expr.SimpleName
+    comments.Comment
+    ;; comments.BlockComment
+    ;; comments.JavadocComment
+    ;; comments.LineComment
     ]
    ))
 (defmethod to-clj String [s] s)
 
 (defmethod to-clj SimpleName [x]
   (.asString x))
+
+(defmethod to-clj Comment [c]
+  (format ";; %s" (.getContent c)))
+
+;;(defmethod to-clj BlockComment [c] :Comment)
+;;(defmethod to-clj JavadocComment [c] :Comment)
+;;(defmethod to-clj LineComment [c] :Comment)
+
 
 (defmethod to-clj VariableDeclarator [vd]
   (let [n (.getName vd)
