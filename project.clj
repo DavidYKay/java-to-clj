0ee35b8ec9fc1abbc4adadd4d00399fbe1c6d11e
David Young-chan Kay <davidykay@gmail.com>
2021-08-01 14:50:06 -0700

    Updated lein-ring to 0.12.5

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject java-to-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Convert Java source to Clojure source"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [com.github.javaparser/javaparser-core "3.6.20"]
 
                  [cljfmt "0.6.1"]
@@ -12,7 +11,7 @@
                  [compojure "1.6.1"]
                  [hiccup "1.0.5"]]
 
-  :plugins [[lein-ring "0.12.4"]]
+  :plugins [[lein-ring "0.12.5"]]
   :ring {:handler java-to-clj.web.server/app}
 
   :repl-options {:init-ns java-to-clj.core}
